fc0c5cdb009a03964a3ffb8930327d1172e26dca
Yu <christine.tsai.yu@gmail.com>
2026-03-05 13:40:09 +0800

    add one test cases

--- ecommerce-frontend/src/utils/money.test.ts
+++ ecommerce-frontend/src/utils/money.test.ts
@@ -1,13 +1,14 @@
-import {it, expect, describe} from 'vitest';
-import {formatMoney} from './money';
+import { it, expect, describe } from 'vitest';
+import { formatMoney } from './money';
 
 describe('formatMoney', () => {
     it('formats 1999 cents as $19.99', () => {
         expect(formatMoney(1999)).toBe('$19.99');
     })
 
-    it ('display two decimals', ()=> {
-        expect (formatMoney (2000)).toBe ('$20.00');
-        expect (formatMoney (900)).toBe ('$9.00');
+    it('display two decimals', () => {
+        expect(formatMoney(2000)).toBe('$20.00');
+        expect(formatMoney(900)).toBe('$9.00');
+        expect(formatMoney(23)).toBe('$0.23');
     })
 });
